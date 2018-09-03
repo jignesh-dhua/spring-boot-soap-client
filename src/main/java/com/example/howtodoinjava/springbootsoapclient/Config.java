eed8ef14ea74a05940734edbef56cc8f7609dadc
Jignesh Dhua <Jignesh.Dhua.contractor@oup.com>
2018-09-03 16:06:02 +0100

    Test

--- src/main/java/com/example/howtodoinjava/springbootsoapclient/Config.java
+++ src/main/java/com/example/howtodoinjava/springbootsoapclient/Config.java
@@ -56,7 +56,7 @@ public class Config {
 		
 		
 		KeyStore trustStore = KeyStore.getInstance("JKS");
-		trustStore.load(new FileInputStream("/home/ubuntu//cert.pfx"), "damith".toCharArray());
+		trustStore.load(new FileInputStream("/home/ubuntu/trust_store.jks"), "damith".toCharArray());
 
 		TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
 		tmf.init(trustStore);
