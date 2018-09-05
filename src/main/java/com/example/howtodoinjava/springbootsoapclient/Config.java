bf67de92d9c4ce97e07675d400ec1bf02c465568
Jignesh Dhua <Jignesh.Dhua.contractor@oup.com>
2018-09-05 12:29:35 +0100

    Test

--- src/main/java/com/example/howtodoinjava/springbootsoapclient/Config.java
+++ src/main/java/com/example/howtodoinjava/springbootsoapclient/Config.java
@@ -3,26 +3,26 @@ package com.example.howtodoinjava.springbootsoapclient;
 import java.io.File;
 import java.io.FileInputStream;
 import java.security.KeyStore;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.KeyManager;
 import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
 
-import org.apache.http.HttpHost;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.conn.ssl.NoopHostnameVerifier;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
-import org.springframework.ws.client.core.WebServiceTemplate;
 import org.springframework.ws.client.support.interceptor.ClientInterceptor;
 import org.springframework.ws.soap.security.wss4j2.Wss4jSecurityInterceptor;
 import org.springframework.ws.soap.security.wss4j2.support.CryptoFactoryBean;
-import org.springframework.ws.transport.http.HttpComponentsMessageSender;
 import org.springframework.ws.transport.http.HttpsUrlConnectionMessageSender;
 
 @Configuration
@@ -47,25 +47,25 @@ public class Config {
 		client.setInterceptors(new ClientInterceptor[] {wss4jSecurityInterceptor()});
 		
 		
-		KeyStore clientStore = KeyStore.getInstance("JKS");
-		clientStore.load(new FileInputStream(new File("/home/ubuntu/development_companyinfo_bridgefund_nl_p7b.jks")), "damith".toCharArray());
-		KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
-		kmf.init(clientStore, "damith".toCharArray());
-		
-		KeyManager[] kms = kmf.getKeyManagers();
-		
-		
-		KeyStore trustStore = KeyStore.getInstance("JKS");
-		trustStore.load(new FileInputStream("/home/ubuntu/development_companyinfo_bridgefund_nl_p7b.jks"), "damith".toCharArray());
-
-		TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-		tmf.init(trustStore);
-		TrustManager[] tms = tmf.getTrustManagers();
-		
-		 HttpsUrlConnectionMessageSender sender = new HttpsUrlConnectionMessageSender();
-		 sender.setTrustManagers(new TrustManager[] {new HttpsTrustManager()});
-		 //sender.setTrustManagers(tms);
-		 sender.setKeyManagers(kms);
+//		KeyStore clientStore = KeyStore.getInstance("JKS");
+//		clientStore.load(new FileInputStream(new File("/home/ubuntu/development_companyinfo_bridgefund_nl_p7b.jks")), "damith".toCharArray());
+//		KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+//		kmf.init(clientStore, "damith".toCharArray());
+//		
+//		KeyManager[] kms = kmf.getKeyManagers();
+//		
+//		
+//		KeyStore trustStore = KeyStore.getInstance("JKS");
+//		trustStore.load(new FileInputStream("/home/ubuntu/development_companyinfo_bridgefund_nl_p7b.jks"), "damith".toCharArray());
+//
+//		TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+//		tmf.init(trustStore);
+//		TrustManager[] tms = tmf.getTrustManagers();
+//		
+//		 HttpsUrlConnectionMessageSender sender = new HttpsUrlConnectionMessageSender();
+//		 sender.setTrustManagers(new TrustManager[] {new HttpsTrustManager()});
+//		 //sender.setTrustManagers(tms);
+//		 sender.setKeyManagers(kms);
 		 //sender.setHostnameVerifier(NoopHostnameVerifier.INSTANCE);
 
 		    
@@ -107,9 +107,13 @@ public class Config {
         
 		
 		
+		wss4jSecurityInterceptor.setSecurementSignatureParts(
+	                "{Element}{http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd}Timestamp;" +
+	                        "{Element}{http://schemas.xmlsoap.org/soap/envelope/}Body"
+	        );
 		
 		
-		wss4jSecurityInterceptor.setValidationActions("Signature");
+		wss4jSecurityInterceptor.setValidationActions("Signature Timestamp");
 		
 		wss4jSecurityInterceptor.setValidationDecryptionCrypto(cryptoFactoryBean.getObject());
 		wss4jSecurityInterceptor.setValidationSignatureCrypto((cryptoFactoryBean.getObject()));
@@ -119,4 +123,33 @@ public class Config {
 		
 		return wss4jSecurityInterceptor;
 	}
+	
+	 @Bean
+	    public Boolean disableSSLValidation() throws Exception {
+	        final SSLContext sslContext = SSLContext.getInstance("TLS");
+
+	        sslContext.init(null, new TrustManager[]{new X509TrustManager() {
+	            @Override
+	            public void checkClientTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+	            }
+
+	            @Override
+	            public void checkServerTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+	            }
+
+	            @Override
+	            public X509Certificate[] getAcceptedIssuers() {
+	                return new X509Certificate[0];
+	            }
+	        }}, null);
+
+	        HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory());
+	        HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+	            public boolean verify(String hostname, SSLSession session) {
+	                return true;
+	            }
+	        });
+
+	        return true;
+	    }
 }
